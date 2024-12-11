865e206c5206490442666b44140642ba972e4b1f
Maksym <klumenko2007maks@gmail.com>
2024-12-11 18:46:14 +0100

    Added Search_by_name

--- Products/urls.py
+++ Products/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.check_all_products, name='index'),
     path('product/<int:id>/', views.product_detail, name='product_detail'),
-    path('search_products/', views.search_by_name, name='search_products'),
+    path('search_by_name/', views.search_by_name, name='search_by_name'),
 ]
--- Products/views.py
+++ Products/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-# Check all the products
 def check_all_products(request):
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
 
-# Check details of product
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     return render(request, 'product_detail.html', {'product': product})
 
-# Searching products by name
 def search_by_name(request):
     query = request.GET.get('q')
     if query:
         products = Product.objects.filter(title__icontains=query)
     else:
         products = Product.objects.none()
-    return render(request, 'search_products.html', {'products': products, 'query': query})
+    return render(request, 'search_by_name.html', {'products': products, 'query': query})
